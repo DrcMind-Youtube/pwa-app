3313f96b14d6f0731d37301948ff251299e7800d
Aksanti Bahiga Tacite <tacite.bahiga@gmail.com>
2024-04-17 06:41:47 +0200

    Fix : vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,14 +1,2 @@
-import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
-export default defineConfig({
-  plugins: [
-    VitePWA({
-      registerType: "autoUpdate",
-      injectRegister: "inline",
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-      },
-      manifest: false,
-    }),
-  ],
-});
+export default defineConfig({});
